ef66dde28910096d6d3342c91fd4eb7bb0983b7e
br1992 <br1992@DESKTOP-SKO6KI7>
2023-12-05 16:44:16 -0600

    Initial commit

---
+++ src/main/scala/com/github/br1992/Concurrency.scala
@@ -0,0 +1,40 @@
+package com.github.br1992
+
+import zio.stm.{STM, TRef}
+
+class Concurrency {
+
+  def bothTransactions(var1: TRef[Int], var2: TRef[Int], var3: TRef[Int], var4: TRef[Int]): STM[Throwable, Unit] = {
+    for {
+      _ <- transaction1(var1, var2)
+      _ <- transaction2(var3, var4)
+    } yield ()
+  }
+
+  def transaction1(var1: TRef[Int], var2: TRef[Int]): STM[Throwable, (Int, Int)] = {
+    for {
+      curVar1 <- var1.get
+      curVar2 <- var2.get
+
+      newVar1 = curVar1 + 1
+      newVar2 = curVar2 - 1
+
+      _ <- var1.set(newVar1)
+      _ <- var2.set(newVar2)
+    } yield (newVar1, newVar2)
+  }
+
+  def transaction2(var3: TRef[Int], var4: TRef[Int]): STM[Throwable, (Int, Int)] = {
+    for {
+      curVar3 <- var3.get
+      curVar4 <- var4.get
+
+      newVar3 = curVar3 * 2
+      newVar4 = curVar4 / 2
+
+      _ <- var3.set(newVar3)
+      _ <- var4.set(newVar4)
+    } yield (newVar3, newVar4)
+  }
+
+}
